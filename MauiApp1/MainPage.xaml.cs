2d2bcfc3e783ad6fdae8d4ad6ef79219798cfc1f
alice irs <aliceiris1315@gmail.com>
2025-04-24 16:51:04 -0300

    Adicionar arquivos de projeto.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                double etanol = Convert.ToDouble(txt_etanol.Text);
+                double gasolina = Convert.ToDouble(txt_gasolina.Text);
+
+                string msg = "";
+
+                if (etanol <= (gasolina * 0.7))
+                {
+                    msg = "O etanol esta compensando";
+                }
+                else
+                {
+                    msg = "A gasolina esta compensando";
+                }
+                DisplayAlert("Calculado", msg, "Ok");
+
+            } catch (Exception ex)
+            {
+                DisplayAlert ("Ops", ex.Message, "Fechar");
+
+            }
+        } //FECHA METD
+    }//FECHA O CLASS
+
+}////FECHA NAMESPACE
